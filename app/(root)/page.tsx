82366b6cd4d2eb776c006824bd76d6933054b23b
faith <faith@marascientific.org>
2025-04-09 23:26:47 +0300

    Add components, hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,44 @@
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams:Promise<{query?:string}>
+}) {
+
+  const query =  (await searchParams).query
+
+  const posts = [{
+    _createdAt:"Yesterday",
+    views:55,
+    author:{
+      _id:1,
+    },
+    _id:1,
+     description:"This is a description",
+     image:"https://picsum.photos/seed/picsum/200/300",
+     caategory:"Robots",
+     title:"We Robots"
+  }]
   return (
       <>
-        <h1 className="text-2xl">Home</h1>
+      <section className="pink_container pattern">
+        <h1 className="heading">Pitch your Startup, <br/> Connect with entrepreneurs</h1>
+        <p className="sub-heading max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query}/>
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}`:"All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map(post=>(
+              <div key={post._id}>{post._createdAt}</div>
+            )) 
+          ):(<p className="no-results">No startups found</p>)}
+        </ul>
+      </section>
       </>
   );
 }
