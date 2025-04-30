a070ee7110a54749df609b7c592dd38cf3f2903d
Aigo Faith <aigofaith@gmail.com>
2025-04-30 18:25:35 +0300

    Add shadcn components

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+    };
+  }
+  interface JWT{
+    id:string
+  }
+}
